760d4298929bc5bc4865c5fc39fc3bbc3c744f62
Ryan Rodwell <ryanrodwell@gmail.com>
2017-08-11 11:57:42 -0400

    attempting to add relationships between tables

--- resolvers.js
+++ resolvers.js
@@ -1,10 +1,11 @@
 export default {
   Query: {
-    getHeisman: (parent, { School }, { db }) => db.Heisman.findOne({
-      where: {
-        School,
-      },
-    }),
+
+    // getHeisman: (parent, { School }, { db }) => db.Heisman.findOne({
+    //   where: {
+    //     School,
+    //   },
+    // }),
     // getHeismans: (parent, { Count }, { db }) => db.Heisman.findAll({
     //   where: {
     //     Trophies: Count,
--- schema.js
+++ schema.js
@@ -11,7 +11,24 @@ export default `
     # Any special comments about the trophies(looking at you USC)
     Special: String!
   } 
+  
+  type HeismanList {
+    id: ID!
+    Year: Int!
+    Winner: String!
+    School: String!
+    Position: String!
+    Points: Int!
+    PercentPointsPossible: String!
+    Heisman: Heisman!
+  }
 
+  
+  type RootQuery {
+    HeismanList: HeismanList!
+    Heisman: Heisman!
+  }
+  
   type Query {
     # Search by school for basic Heisman information
     getHeisman(School: String!): Heisman
