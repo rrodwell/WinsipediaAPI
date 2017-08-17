65610629eb065874331e42ef125a4cfc2c5ef2f0
Les Barchard <lbarchard@Less-MacBook-Pro.local>
2017-08-16 21:47:04 -0400

    Add resolver for all americans

--- resolvers.js
+++ resolvers.js
@@ -1,30 +1,36 @@
 export default {
   Query: {
     team: (parent, args, source) => source.db.Team.findOne({
-        attributes: ['TeamId', 'School', 'Name', 'Conference', 'Coach', 'Location', 'State', 'Abbreviation'],
-        where: {
-          School: args.School,
-        },
+      attributes: ['TeamId', 'School', 'Name', 'Conference', 'Coach', 'Location', 'State', 'Abbreviation'],
+      where: {
+        School: args.School,
+      },
     }),
   },
   Team: {
-      Heisman: (parent, args, source) => source.db.Heisman.findOne({
-          where: {
-              School: parent.School,
-          },
-      }),
-      SocialMedia: (parent, args, source) => source.db.Team.findOne({
-          attributes: ['ADTwitter', 'FBTwitter', 'SBNTwitter', 'URL'],
-          where: {
-              School: parent.School,
-          },
-      }),
-      Branding: (parent, args, source) => source.db.Team.findOne({
-          attributes: ['Color', 'SecondaryColor', 'LikeColor'],
-          where: {
-              School: parent.School,
-          },
-      }),
+    Heisman: (parent, args, source) => source.db.Heisman.findOne({
+      where: {
+        School: parent.School,
+      },
+    }),
+    SocialMedia: (parent, args, source) => source.db.Team.findOne({
+      attributes: ['ADTwitter', 'FBTwitter', 'SBNTwitter', 'URL'],
+      where: {
+        School: parent.School,
+      },
+    }),
+    Branding: (parent, args, source) => source.db.Team.findOne({
+      attributes: ['Color', 'SecondaryColor', 'LikeColor'],
+      where: {
+        School: parent.School,
+      },
+    }),
+    AllAmerican: (parent, args, source) => source.db.AllAmerican.findOne({
+      attributes: ['Number', 'Players'],
+      where: {
+        School: parent.School,
+      },
+    }),
   },
   Heisman: {
     HeismanWinners: (parent, args, source) => source.db.HeismanList.findAll({
