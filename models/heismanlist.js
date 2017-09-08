4dfe33b91ed06014b137a767e8cdb2fb3fe129a8
Ryan Rodwell <ryanrodwell@gmail.com>
2017-09-08 14:05:54 -0400

    attempting to pull from master/update files

--- models/heismanlist.js
+++ models/heismanlist.js
@@ -1,17 +1,19 @@
+/**
+ * Created by ryanrodwell on 8/11/17.
+ */
 export default (sequelize, DataTypes) => {
-  const HeismanList = sequelize.define('HeismanList', {
-    Id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-    },
-    Year: DataTypes.INTEGER,
-    Winner: DataTypes.STRING,
-    School: DataTypes.STRING,
-    Position: DataTypes.STRING,
-    Points: DataTypes.INTEGER,
-    PercentPointsPossible: DataTypes.FLOAT,
-    createdAt: DataTypes.STRING,
-    updatedAt: DataTypes.STRING,
-  });
-  return HeismanList;
+    const HeismanList = sequelize.define('HeismanList', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+        },
+        Year: DataTypes.INTEGER,
+        Winner: DataTypes.STRING,
+        School: DataTypes.STRING,
+        Position: DataTypes.STRING,
+        Points: DataTypes.INTEGER,
+        PercentPointsPossible: DataTypes.STRING
+    });
+
+    return HeismanList;
 };
