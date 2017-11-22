57b664580deb85b5b32e4e850d7d339cb28153e9
Les Barchard <lbarchard@Less-MacBook-Pro.local>
2017-11-22 12:06:02 -0500

    made changes to be async throughout resolvers and put try catch around all resolvers.

--- api/resolvers.js
+++ api/resolvers.js
@@ -1,11 +1,44 @@
 export default {
   Query: {
-    team: (parent, args, source) => source.db.Team.findOne({
-      attributes: ['TeamId', 'School', 'Name', 'Conference', 'Coach', 'Location', 'State', 'Abbreviation'],
-      where: {
-        School: args.School,
-      },
-    }),
+    team: async (parent, args, source) => {
+      try {
+        let schoolID = await source.db.Team.findOne({
+          attributes: ['TeamId', 'School'],
+          where: {
+            Slug: args.School,
+          },
+        })
+        let teamInfo = await source.db.Team.findOne({
+          attributes: ['TeamId', 'School', 'Name', 'Conference', 'Coach', 'Location', 'State', 'Abbreviation'],
+          where: {
+            TeamID: schoolID.dataValues.TeamId,
+            },
+          }) 
+        return {
+          TeamId: teamInfo.dataValues.TeamId,
+          School: teamInfo.dataValues.School,
+          Name: teamInfo.dataValues.Name,
+          Conference: teamInfo.dataValues.Conference,
+          Coach: teamInfo.dataValues.Coach,
+          Location: teamInfo.dataValues.Location,
+          State: teamInfo.dataValues.State,
+          Abbreviation: teamInfo.dataValues.Abbreviation,
+        }
+      }
+      catch (e) {
+        return {
+          TeamId: "",
+          School: "",
+          Name: "",
+          Conference: "",
+          Coach: "",
+          Location: "",
+          State: "",
+          Abbreviation: "",
+        }
+      }
+    },
+
     matchup: async (parents, args, source) => {
       try {
         let school1ID = await source.db.Team.findOne({
@@ -45,36 +78,112 @@ export default {
       }
     }
   },
+
   Team: {
-    Heisman: (parent, args, source) => source.db.Heisman.findOne({
-      where: {
-        School: parent.School,
-      },
-    }),
-    SocialMedia: (parent, args, source) => source.db.Team.findOne({
-      attributes: ['ADTwitter', 'FBTwitter', 'SBNTwitter', 'URL'],
-      where: {
-        School: parent.School,
-      },
-    }),
-    Branding: (parent, args, source) => source.db.Team.findOne({
-      attributes: ['Color', 'SecondaryColor', 'LikeColor'],
-      where: {
-        School: parent.School,
-      },
-    }),
-    AllAmerican: (parent, args, source) => source.db.AllAmerican.findOne({
-      attributes: ['Number', 'Players'],
-      where: {
-        School: parent.School,
-      },
-    }),
+    Heisman: async (parent, args, source) => {
+      try {
+        let heismanInfo = await source.db.Heisman.findOne({
+          attributes: ['School','Trophies','Special'],  
+          where: {
+            TeamId: parent.TeamId,
+          }
+        })
+        return {
+          Trophies: heismanInfo.dataValues.Trophies,
+          Special: heismanInfo.dataValues.Special,
+          School: heismanInfo.dataValues.School
+        }
+      }
+      catch (e) {
+        return {
+          Trophies: 0,
+          Special: "",
+        }    
+      }
+    },
+    SocialMedia: async (parent, args, source) => {
+      try {
+        let socialMediaInfo = await source.db.Team.findOne({
+          attributes: ['ADTwitter', 'FBTwitter', 'SBNTwitter', 'URL'],
+          where: {
+            TeamId: parent.TeamId,
+          }
+        })
+        return {
+          ADTwitter: socialMediaInfo.dataValues.ADTwitter,
+          FBTwitter: socialMediaInfo.dataValues.FBTwitter,
+          SBNTwitter: socialMediaInfo.dataValues.SBNTwitter,
+          URL: socialMediaInfo.dataValues.URL,
+        }
+      }
+      catch (e) {
+        return {
+          ADTwitter: "",
+          FBTwitter: "",
+          URL: "",
+        }
+      }
+    },
+    Branding: async (parent, args, source) => {
+      try {
+        let brandingInfo = await source.db.Team.findOne({
+          attributes: ['Color', 'SecondaryColor', 'LikeColor'],
+          where: {
+            TeamId: parent.TeamId,
+          },
+        })
+        return {
+          Color: brandingInfo.dataValues.Color,
+          SecondaryColor: brandingInfo.dataValues.SecondaryColor,
+          LikeColor: brandingInfo.dataValues.LikeColor,
+        }
+      }
+      catch (e) {
+        return {
+          Color: "",
+          SecondaryColor: "",
+          LikeColor: "",
+        }
+      }
+    },
+    AllAmerican: async (parent, args, source) => {
+      try {
+        let allAmericanInfo = await source.db.AllAmerican.findOne({
+          attributes: ['Number', 'Players'],
+          where: {
+            TeamId: parent.TeamId,
+          },
+        })
+        return {
+          Number: allAmericanInfo.dataValues.Number,
+          Players: allAmericanInfo.dataValues.Players,
+        }
+      }
+      catch (e) {
+        return {
+          Number: 0,
+          Players: 0,
+        }
+      }
+    },
   },
   Heisman: {
-    HeismanWinners: (parent, args, source) => source.db.HeismanList.findAll({
-      where: {
-        School: parent.School,
-      },
-    }),
+    HeismanWinners: async (parent, args, source) => {
+      console.log ("HELLOWORLD")
+      console.log(parent)
+      console.log ("GOODBYEWORLD")
+      try {
+        let heismanWinnersInfo = await source.db.HeismanList.findAll({
+          attributes: ['Year', 'Winner', 'Position', 'Points', 'PercentPointsPossible'],          
+          where: {
+            School: parent.School,
+          },
+        })
+        return heismanWinnersInfo
+      }
+      catch (e) {
+        return []
+      }
+    },
   },
 };
