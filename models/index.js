ed30ce626cc631646400b20aa7a3067a7d4bbcf8
Ryan Rodwell <ryanrodwell@gmail.com>
2018-02-01 09:00:04 -0500

    deploying and connecting to jaws DB

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@
 
 var Sequelize = require('sequelize');
 var env       = process.env.NODE_ENV || 'development';
-var config    = require('./config.json')[env];
+var config = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
 config.define = {
